3694c65a8137c8a01825a94f450f9d62f83345a4
agustveee <trixiavanessatorrejos04@gmail.com>
2025-03-17 16:13:58 +0800

    javascript

---
+++ character builder/script.js
@@ -0,0 +1,14 @@
+document.querySelectorAll('.armorpiece').forEach(item => {
+    item.addEventListener('click', function () {
+        const part = this.getAttribute('data-part');
+        const selectedImage = this.getAttribute('src');
+
+        document.getElementById(part).setAttribute('src', selectedImage);
+
+        document.querySelectorAll(`.armorpiece[data-part='${part}']`).forEach(el =>
+            el.classList.remove('selected')
+        );
+
+        this.classList.add('selected');
+    });
+});
